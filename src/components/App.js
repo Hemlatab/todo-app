0fe6c3fd2391d018af7cb026f1a5b8b013b99c0e
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-10 23:16:16 +0400

    Create home content for todo-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
- 
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+ import TodoItem from "./TodoItem"
+
+
   function App (){
     return(
-      <div className="App">
-     <h1>Todo App</h1>
+      <div className="todo-list">
+        {/* <Header />
+        <MainContent />
+        <Footer /> */}
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
       </div>
     )
   }
---
+++ src/components/MainContent.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Header from "./Header"
+import Footer from "./Footer"
+
+function MainContent(){
+    return(
+     <div>
+         
+     <input type="checkbox" />
+     <p>Placeholder text here</p>
+
+     <input type="checkbox" />
+     <p>Placeholder text here</p>
+     </div>
+    )
+}
+
+export default MainContent
